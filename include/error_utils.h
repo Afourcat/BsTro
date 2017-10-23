dce8454a8a71821470665ebecb0219271eafb564
Alexandre FOURCAT <afourcat@linux.home>
2017-10-23 21:32:05 +0200

    origin push

---
+++ include/error_utils.h
@@ -0,0 +1,14 @@
+ /*
+ ** EPITECH PROJECT, 2017
+ ** error_utils.h
+ ** File description:
+ **
+ */
+
+#ifndef ERROR_UTILS_H_
+#define ERROR_UTILS_H_
+
+int my_putchar_error(char c);
+int my_putstr_error(char *s);
+
+#endif /* ERROR_UTILS_H_ */
