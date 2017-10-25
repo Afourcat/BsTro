fb653ade366114f9e0b3cf4095fda3eebc429d5e
Thomas Nicollet <thomas.nicollet@epitech.eu>
2017-10-25 11:18:09 +0200

    New commit

--- include/utils.h
+++ include/utils.h
@@ -13,5 +13,6 @@ void my_putstr(char *str);
 int my_strlen(char const *str);
 char *my_strcat(char *dest, char *src);
 int my_atoi(char const *str);
+int is_in(char c, char *str);
 
 #endif /* UTILS_H_ */
