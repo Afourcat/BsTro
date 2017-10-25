147ada1ac67b25846e5cd8b8bd2ea01fdb7ef65a
Thomas Nicollet <thomas.nicollet@epitech.eu>
2017-10-25 14:46:00 +0200

    Modified mul function

---
+++ src/convert_base/convert_base.c
@@ -0,0 +1,14 @@
+/*
+** EPITECH PROJECT, 2017
+** convert_base
+** File description:
+** Main file for functions that converts base to base
+*/
+
+#include <stdlib.h>
+#include <convert_base.h>
+
+char *convert_base(char *src, char *base_to, char *base_from)
+{
+	return (temp);
+}
