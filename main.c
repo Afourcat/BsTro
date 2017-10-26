b68ead22831753f6f8958ed5e7224e8e53a93af2
Thomas Nicollet <thomas.nicollet@epitech.eu>
2017-10-26 10:52:07 +0200

    Fixed some things in the postfix function

--- main.c
+++ main.c
@@ -4,16 +4,11 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-int main(void)
+int main(int ac, char **av)
 {
-	char *temp = postfix("1-2*7/((8+4-1)*(1-2))", "+-*/", "0123456789", "()");
+	char *temp = postfix(av[1], "+-*/", "0123456789", "()");
 	int counter = 0;
 	
-	while (temp[counter]) {
-		printf("%d ", temp[counter]); 
-		counter++;
-	}
-	my_putchar('\n');
 	my_putstr(temp);
 	free(temp);
 	return (0);
