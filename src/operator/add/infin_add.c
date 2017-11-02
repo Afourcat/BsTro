3df8afa7bff66ed2edd9b3708771b8c422289061
Alexandre FOURCAT <afourcat@localhost.localdomain>
2017-11-02 16:34:47 +0100

    add fix wip

--- src/operator/add/infin_add.c
+++ src/operator/add/infin_add.c
@@ -17,8 +17,11 @@ int infin_add_wrapped(char **nb1, char **nb2, int size, char *result_string)
 
 	*nb1 = remove_minus(*nb1);
 	*nb2 = remove_minus(*nb2);
-	if (signe_1 == -1 && signe_2 != -1 ||
-		 signe_2 == -1 && signe_1 != -1) {
+	if (signe_1 == -1 && signe_2 == -1) {
+		add_it(*nb1, *nb2, result_string);
+		bool_s = (*nb1[0] == '0' && *nb2[0] == '0' ? 0 : 1);
+	}
+	else if (signe_1 == -1 || signe_2 == -1) {
       		if (compare(*nb1, *nb2) == 1)
 			bool_s = sub_it(*nb1, *nb2, result_string, signe_1);
 		else
