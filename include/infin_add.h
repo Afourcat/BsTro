776d8839138ab561a7bd374834ee774468b272ba
Guibert Vinchenzo <vinchenzo.guibert@epitech.eu>
2017-11-03 12:06:00 +0100

    Fixing compilation multiplication

--- include/infin_add.h
+++ include/infin_add.h
@@ -8,10 +8,11 @@
 #ifndef INFIN_ADD_H_
 #define INFIN_ADD_H_
 
-int get_sign_add(char **str);
+int get_sign_ope(char **str);
 void add_it(char *nb1, char *nb2, char *res);
 int infin_add_wrapped(char **nb1, char **nb2, char *res);
 char *infin_add(char *nb1, char *nb2);
+void free_sign_ope(char *nb1, char *nb2, int sign_nb1, int sign_nb2);
 void sub_it(char *nb1, char *nb2, char *res, int *sizes);
 int sub(char *nb1, char *nb2, char *res, int sign);
 
