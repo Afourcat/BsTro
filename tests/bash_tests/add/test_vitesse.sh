80f6eee9b5e3e4d963679ffc44a198e3ed3b9e7b
Thomas Nicollet <thomas.nicollet@epitech.eu>
2017-11-03 13:32:56 +0100

    Moved tests to another repo to prevent the moulinette to find any coding style faults

--- tests/bash_tests/add/test_vitesse.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-for i in `seq 1 1000`; do
-
-    nombre_1=$(cat /dev/urandom | tr -dc '0-9' | fold -w 256 | head -n 1 | sed -e 's/^0*//' | head --bytes $(( ( RANDOM % 1000000 )  + 1 )))
-    nombre_2=$(cat /dev/urandom | tr -dc '0-9' | fold -w 256 | head -n 1 | sed -e 's/^0*//' | head --bytes $(( ( RANDOM % 1000000 )  + 1 )))
-    ./add/infin_add $nombre_1 $nombre_2
-done
